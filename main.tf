65bdd4aee9a0bd1e69af41253837ef5ea7aa5919
nasinghn <33350388+nasinghn@users.noreply.github.com>
2022-11-12 17:12:03 +0000

    added instance detail and output block

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "aws_s3_bucket" "tf_course" {
   bucket = "nasinghn-test-bucket"
 #  acl = "private"
 }
+resource "aws_instance" "test_instance" {
+   ami           = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+output "app_server" {
+  value = "${aws_instance.app_server.public_ip}"
+}
