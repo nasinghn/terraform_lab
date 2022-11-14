fba4d2eb219f5dcf63ddfd49cca6e54cdba5e17f
nasinghn <33350388+nasinghn@users.noreply.github.com>
2022-11-14 15:26:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = $access_key
-  secret_key = $secret_key
-  region     = us-east-2
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 resource "aws_s3_bucket" "tf_course" {
   bucket = "nasinghn-test-bucket"
